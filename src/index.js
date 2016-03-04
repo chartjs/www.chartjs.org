27c962001e7a7565196d1326c97ea3a1e64dc255
Nick Downie <hello@nickdownie.com>
2016-03-04 11:47:35 +0000

    Add in animated bubble chart

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import backgroundBars from './charts/backgroundBars';
 // import chartLogo from './charts/chartLogo';
 import mixedTypes from './charts/mixedTypes';
 import axisTypes from './charts/axisTypes';
+import animatedChart from './charts/animatedChart';
 
 
 backgroundBars(document.getElementById('background-bar').getContext('2d'));
 // chartLogo(document.getElementById('logo-chart').getContext('2d'));
 mixedTypes(document.getElementById('mixed-chart').getContext('2d'));
 axisTypes(document.getElementById('axis-chart').getContext('2d'));
+animatedChart(document.getElementById('animate-chart').getContext('2d'));
