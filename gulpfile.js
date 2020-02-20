48a47bc1f792634f57179f45908ca9f0de2a1c36
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-02-20 10:26:58 -0800

    Add jekyll config and update dependencies (#7)

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var merge = require('merge-stream');
 
 gulp.task('build-js', function() {
 	return browserify({entries: './src/index.js', debug: gutil.env.debug})
-		.transform(babel, {presets: ['es2015']})
+		.transform(babel, {presets: ['@babel/preset-env']})
 		.bundle()
 		.pipe(source('build.js'))
 		.pipe(buffer())
@@ -25,19 +25,12 @@ gulp.task('build-js', function() {
 		.pipe(gulp.dest('./www'));
 });
 
-gulp.task('watch-js', ['build-js'], function() {
+gulp.task('watch-js', gulp.series('build-js', function() {
 	return gulp.watch(
 		'./src/*.js',
 		['build-js']
 	);
-});
-
-gulp.task('server', ['site'], function() {
-	connect.server({
-		root: 'www',
-		port: 8080
-	});
-});
+}));
 
 gulp.task('less', function() {
 	return gulp.src('./src/styles.less')
@@ -49,35 +42,26 @@ gulp.task('less', function() {
 		}))
 		.pipe(less())
 		.pipe(autoprefixer({
-			browsers: ['last 2 version'],
 			cascade: false
 		}))
 		.pipe(gulp.dest('./www'));
 });
 
-gulp.task('watch-less', ['less'], function() {
+gulp.task('watch-less', gulp.series('less', function() {
 	return gulp.watch(
 		'./src/*.less',
 		['less']
 	);
-});
-
-gulp.task('watch-templates', ['template'], function() {
-	return gulp.watch(
-		'./src/**/*.html',
-		['template']
-	);
-});
+}));
 
 gulp.task('copy-assets', function() {
 	return merge(
+		gulp.src('_config.yml').pipe(gulp.dest('./www')),
 		gulp.src('./src/favicon.ico').pipe(gulp.dest('./www')),
 		gulp.src('./src/img/*').pipe(gulp.dest('./www/img')),
 		gulp.src('./media/*').pipe(gulp.dest('./www/media'))
 	);
 });
-
-gulp.task('template', ['docs-template', 'samples-template', 'home-template']);
 
 gulp.task('docs-template', function() {
 	return gulp.src('./src/templates/docs.html')
@@ -100,6 +84,22 @@ gulp.task('home-template', function() {
 		.pipe(gulp.dest('./www'));
 });
 
-gulp.task('site', ['template', 'less', 'build-js', 'copy-assets']);
+gulp.task('template', gulp.series('docs-template', 'samples-template', 'home-template'));
 
-gulp.task('default', ['server', 'watch-js', 'watch-less', 'watch-templates']);
+gulp.task('watch-templates', gulp.series('template', function() {
+	return gulp.watch(
+		'./src/**/*.html',
+		['template']
+	);
+}));
+
+gulp.task('site', gulp.series('template', 'less', 'build-js', 'copy-assets'));
+
+gulp.task('server', gulp.series('site', function() {
+	connect.server({
+		root: 'www',
+		port: 8080
+	});
+}));
+
+gulp.task('default', gulp.series('server', 'watch-js', 'watch-less', 'watch-templates'));
